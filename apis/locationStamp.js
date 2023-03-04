35ffe6558b3e13913cded45fe110621b990cf99d
anisirji <www.allinoneforyouhere@gmail.com>
2023-03-04 11:12:30 -0500

    locationStamoSync

--- apis/locationStamp.js
+++ apis/locationStamp.js
@@ -2,18 +2,24 @@ const { writeDb } = require("../dbUtil");
 
 async function locationStamp(data) {
   console.log(data);
-  const { employee_id, latitude, longitude } = data;
+  const { employee_id, latitude, longitude, timestamp, flag_value } = data;
   const db_table = "txn_geolocation";
 
-
   try {
-    const fields = [ 	"employee_id",	"latitude",	"longitude",	"timestamp",	"flag_value"];
+    const fields = [
+      "employee_id",
+      "latitude",
+      "longitude",
+      "timestamp",
+      "flag_value",
+    ];
     const values = [
       employee_id,
       latitude,
       longitude,
-      new Date().toISOString().slice(0, 19).replace("T", " "),
-      "FlagValue"
+      timestamp,
+      //new Date().toISOString().slice(0, 19).replace("T", " "),
+      flag_value,
     ];
     const createCus = await writeDb(db_table, fields, values);
     if (createCus.flag) {
